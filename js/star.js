c72cbf0320e245795c18684f420a0f2eb7579740
SashaZhardetskaya <sasha310209@gmail.com>
2017-02-19 19:36:05 +0200

    services start js

--- js/star.js
+++ js/star.js
@@ -1,6 +1,7 @@
 
 
 $(document).ready(function(){
+	//top navigation
 	$(window).scroll(function(){
 		if (document.body.scrollTop >= 30) {
 			$("#nav-main").css({
@@ -14,5 +15,31 @@ $(document).ready(function(){
 				'background': 'transparent'
 			});
 		};
+	});
+
+ //    //services accardion
+	// $('.service-icon a').on('click', function(){
+	// 	//next()  получает след-й за текущим элемент
+	// 	//prev()  получает пред-й за текущим элемент
+	// 	$('.service-description').not(this).next().slideUp(700);
+	// 	$(this).next().slideToggle(1000);
+	// 	$('.service-description').not(this).css('display','block');
+	// 	$(this).css('display','block');
+	// });
+});
+
+
+$(document).ready(function(){ //services accardion
+	$('.service-description').eq(0).css('display','block');
+	$('.service-icon a').eq(0).css('background','green');
+	
+	$('.service-icon a').on('click', function(){
+		var a =$('.service-icon a').index(this);
+		// console.log(a);
+		//.eq- оставляет только элемент с порядковым номером а
+		$('.service-description').css('display','none')
+		$('.service-description').eq(a).css('display','block');
+		$('.service-icon a').css('background','gold');
+		$(this).css('background','green');
 	});
 });
